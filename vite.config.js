211a9404cca95b4744c3c7c6da7ae1caafc014a3
supraja <paladisupraja078@gmail.com>
2026-02-15 12:06:59 +0530

    fix9

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  
+  server: {
+    proxy: {
+      '/prod': 'http://20.244.40.188'
+    }
+  }
+,
   plugins: [react()],
 })
