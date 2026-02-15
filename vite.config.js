e82a553cf718cda1bdbb8baae1cdf1e36c468f85
supraja <paladisupraja078@gmail.com>
2026-02-15 11:29:45 +0530

    fix4

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://20.244.40.188/dev',
+        changeOrigin: true,
+        secure: false, // allow HTTP
+        rewrite: (path) => path.replace(/^\/api/, '') // remove /api prefix when sending to backend
+      }
+    }
+  }
 })
