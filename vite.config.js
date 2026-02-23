6f7b7bb828d8d6e5d01718f9998490744f4ccf5c
supraja <paladisupraja078@gmail.com>
2026-02-23 14:28:04 +0530

    overalltotalchanges19

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+    
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
-    
   plugins: [react()],
+  server: {
+    proxy: {
+      '/prod': 'http://20.244.40.188',
+    },
+  },
 })
