33a84bb353bc888a6517b7b8f3adb03675179248
supraja <paladisupraja078@gmail.com>
2026-02-23 14:30:43 +0530

    overalltotalchanges20

--- vite.config.js
+++ vite.config.js
@@ -11,9 +11,10 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  base: "/Dairy-Portal/", // important for GitHub Pages
   server: {
     proxy: {
-      '/prod': 'http://20.244.40.188',
+      '/prod': 'http://20.244.40.188', // for development only
     },
   },
 })
